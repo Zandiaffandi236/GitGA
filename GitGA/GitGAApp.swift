2fcef39890fec2a83cb71939227eeb491683fe41
Marzandi Zahran <marzandi.leta@gmail.com>
2026-06-04 09:00:43 +0700

    Initial Commit

---
+++ GitGA/GitGAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitGAApp.swift
+//  GitGA
+//
+//  Created by Marzandi Zahran Affandi Leta on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GitGAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
