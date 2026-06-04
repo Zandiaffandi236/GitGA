ba407a89d260c2db3bb03e17230052923b25c608
Hammam Akmal <hammamakmal.17@gmail.com>
2026-06-04 12:00:34 +0700

    tesst conflict

--- GitGA/LoginPage.swift
+++ GitGA/LoginPage.swift
@@ -15,7 +15,7 @@ struct LoginPage: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Marjan!")
-            Text("Hello, Hammam Login 456")
+            Text("Hello, Hammam Login 789")
                 .foregroundStyle(.red)
                 .font(Font.system(.title, design: .rounded))
                 .bold(true)
